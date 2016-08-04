ab554e40542ec42d43e839ef170836816e7ffc8d
Viacheslav Kliuev <slavkluev@yandex.ru>
2016-08-05 01:50:40 +0700

    Fixed

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -8,8 +8,11 @@ class UtilsTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetFiles()
     {
-        $files = [__DIR__ . "\\Fixtures\\bad.php", __DIR__ . "\\Fixtures\\good.php"];
-        $this->assertEquals($files, Utils::getFiles(__DIR__ . "\\Fixtures"));
+        $files = [
+            __DIR__ . DIRECTORY_SEPARATOR . "Fixtures" . DIRECTORY_SEPARATOR . "bad.php",
+            __DIR__ . DIRECTORY_SEPARATOR . "Fixtures" . DIRECTORY_SEPARATOR . "good.php"
+        ];
+        $this->assertEquals($files, Utils::getFiles(__DIR__ . DIRECTORY_SEPARATOR . "Fixtures"));
     }
 
     public function testGetRules()
